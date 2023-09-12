8aacbbadfe4673acf7e2250a4fe6c5b4a6ad78ff
HelloImSak <140347848+HelloImSak@users.noreply.github.com>
2023-09-12 11:05:11 +0300

    Create Exercise3.cpp

---
+++ Exercise3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double S, p, n;
+
+    cout << "Enter the loan amount (S) in rubles: ";
+    cin >> S;
+
+    cout << "Enter the annual interest rate (p) as a percentage: ";
+    cin >> p;
+
+    cout << "Enter the number of years (n): ";
+    cin >> n;
+
+    // Convert the annual interest rate from percentage to decimal
+    p = p / 100.0;
+
+    // Calculate the monthly payment
+    double monthlyInterestRate = p / 12.0;
+    double denominator = 1 - pow(1 + monthlyInterestRate, -12 * n);
+    double monthlyPayment = S * (monthlyInterestRate / denominator);
+
+    cout << "Monthly payment (m): " << monthlyPayment << " rubles" << endl;
+
+    return 0;
+}
