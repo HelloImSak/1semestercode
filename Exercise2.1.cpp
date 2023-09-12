c926155033007b73e69e8768b35255b9f29ab70c
HelloImSak <140347848+HelloImSak@users.noreply.github.com>
2023-09-12 09:51:33 +0300

    Create Exercise2.1.cpp

---
+++ Exercise2.1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double x, a;
+
+    cout << "Enter the value of x: ";
+    cin >> x;
+
+    cout << "Enter the value of a: ";
+    cin >> a;
+
+    double result;
+
+    if (x < 0) {
+        // Calculate a^ln(|x|) when x is less than 0
+        result = pow(a, log(fabs(x)));
+    } else {
+        // Calculate sqrt(a - x*x) when x is greater than or equal to 0
+        result = sqrt(a - x * x);
+    }
+
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
