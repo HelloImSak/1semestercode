7595e7e50ab5678464fe4bd2b6b10bcc11fff1bb
HelloImSak <140347848+HelloImSak@users.noreply.github.com>
2023-09-15 00:33:46 +0300

    Create Задача «Фильтр».3.3.cpp

---
+++ Задача «Фильтр».3.3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+    string filename = "sample.txt";    
+    ofstream outputFile(filename);
+    if (outputFile.is_open()) {
+        outputFile << "This is other file 11234";
+
+        outputFile.close();
+        ifstream inputFile(filename);
+
+        if (inputFile.is_open()) {
+            string line;
+
+            // Read each line from the file
+            while (getline(inputFile, line)) {
+                // Iterate through each character in the line
+                for (char character : line) {
+                    // Check if the character is a digit
+                    if (isdigit(character)) {
+                        cout << character;
+                    }
+                }
+            }
+            inputFile.close();
+        }
+        else {
+            cout << "Failed to open the file for reading." << endl;
+            return 1;
+        }
+    }
+    return 0;
+}
