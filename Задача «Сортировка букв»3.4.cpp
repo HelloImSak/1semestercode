961b4d427b9e1c807a7dbb1bba6a5b5c86467a1f
HelloImSak <140347848+HelloImSak@users.noreply.github.com>
2023-09-22 01:06:46 +0300

    Update Задача «Сортировка букв»3.4.cpp

--- Задача «Сортировка букв»3.4.cpp
+++ Задача «Сортировка букв»3.4.cpp
@@ -14,3 +14,19 @@ int main() {
 
     return 0;
 }
+-------------------------------------------
+    #include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string input = "aeybnvcdxtwrzpklomqfsuhgj";
+
+    // Сортировка строковой переменной
+    sort(input.begin(), input.end());
+
+    cout << "Отсортированная строка: " << input << endl;
+
+    return 0;
+}
