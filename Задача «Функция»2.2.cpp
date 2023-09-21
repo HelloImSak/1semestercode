61f8c8af52936910a0095d8566a5eff638540ec5
HelloImSak <140347848+HelloImSak@users.noreply.github.com>
2023-09-22 00:31:37 +0300

    Update Задача «Функция»2.2.cpp

--- Задача «Функция»2.2.cpp
+++ Задача «Функция»2.2.cpp
@@ -6,13 +6,22 @@ int main() {
     double x, y, b;
     
     cout << "Enter the value of x: ";
-    cin >> x;
+    if (!(cin >> x)) {
+        cout << "Invalid input for x. Please enter a numeric value." << endl;
+        return 1;  // Exit with an error code
+    }
 
     cout << "Enter the value of y: ";
-    cin >> y;
+    if (!(cin >> y)) {
+        cout << "Invalid input for y. Please enter a numeric value." << endl;
+        return 1;  // Exit with an error code
+    }
 
     cout << "Enter the value of b: ";
-    cin >> b;
+    if (!(cin >> b)) {
+        cout << "Invalid input for b. Please enter a numeric value." << endl;
+        return 1;  // Exit with an error code
+    }
 
     if (b > y && b > x) {
         double result = log(b - y) * sqrt(b - x);
