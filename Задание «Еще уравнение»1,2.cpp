bd425ca8e670aaf65fe1fd61072d8934aac2158a
HelloImSak <140347848+HelloImSak@users.noreply.github.com>
2023-09-22 00:19:09 +0300

    Update Задание «Еще уравнение»1,2.cpp

--- Задание «Еще уравнение»1,2.cpp
+++ Задание «Еще уравнение»1,2.cpp
@@ -75,4 +75,52 @@ int main()
 	 
     return 0;
 	}
+-----------------------------------------------------------------------------------------------------------------
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    float a, b, c, x1, x2, delta;
+
+    cout << "Enter coefficients a, b, and c: ";
+    cin >> a >> b >> c;
+
+    delta = b * b - 4 * a * c;
+
+    if (a == 0) {
+        if (b == 0) {
+            if (c == 0) {
+                cout << "The equation has infinite solutions." << endl;
+            } else {
+                cout << "The equation has no real solutions." << endl;
+            }
+        } else {
+            float root = -c / b;
+            cout << "x = " << root << endl;
+        }
+    } else {
+        if (delta > 0) {
+            x1 = (-b + sqrt(delta)) / (2 * a);
+            x2 = (-b - sqrt(delta)) / (2 * a);
+            cout << "Roots are real and different:" << endl;
+            cout << "x1 = " << x1 << endl;
+            cout << "x2 = " << x2 << endl;
+        } else if (delta == 0) {
+            x1 = -b / (2 * a);
+            cout << "Roots are real and the same:" << endl;
+            cout << "x1 = x2 = " << x1 << endl;
+        } else {
+            float realPart = -b / (2 * a);
+            float imaginaryPart = sqrt(-delta) / (2 * a);
+            cout << "Roots are complex and different:" << endl;
+            cout << "x1 = " << realPart << " + " << imaginaryPart << "i" << endl;
+            cout << "x2 = " << realPart << " - " << imaginaryPart << "i" << endl;
+        }
+    }
+
+    return 0;
+}
+
     
