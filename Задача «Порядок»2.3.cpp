f41eaecfb5837f455cf1faa00dff8b375838966f
HelloImSak <140347848+HelloImSak@users.noreply.github.com>
2023-09-27 22:27:19 +0300

    Update Задача «Порядок»2.3.cpp

--- Задача «Порядок»2.3.cpp
+++ Задача «Порядок»2.3.cpp
@@ -7,15 +7,9 @@ int main() {
     int N;
     cout<<"Input N: ";
     cin>>N;
-    if(N>0){
+    
         for(int i=1;i<=10;i++){
             cout<<N+i<<endl;
-        }
-    }
-    else{
-        cout<<"Invalid input"<<endl;
-        
-    }
-    
+		}
     return 0;
 }
