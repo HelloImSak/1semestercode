b090fe11b7f1999b17e83e7e8d877bab0516dfc4
HelloImSak <140347848+HelloImSak@users.noreply.github.com>
2023-10-22 12:56:24 +0300

    Update Задача «Геометрические фигуры»4.2.cpp

--- Задача «Геометрические фигуры»4.2.cpp
+++ Задача «Геометрические фигуры»4.2.cpp
@@ -9,17 +9,17 @@ double calculateRectangleArea(double length, double width) {
 }
 
 // Function to calculate the area of a triangle
-double calculateTriangleArea(double base, double height) {
-    return 0.5 * base * height;
+double calculateTriangleArea(double a, double b, double c) {
+    double s = (a + b + c) / 2.0;
+    return sqrt(s * (s - a) * (s - b) * (s - c));
 }
-
 // Function to calculate the area of a circle
 double calculateCircleArea(double radius) {
     return M_PI * pow(radius, 2);
 }
 
 int main() {
-    char choice;
+    char choice; 
     
     do{
     cout << "Choose a geometric figure to calculate its area:" << endl;
@@ -40,20 +40,28 @@ int main() {
             break;
         }
         case 2: {
-            double base, height;
-            cout << "Enter the base and height of the triangle: ";
-            cin >> base >> height;
-            double area = calculateTriangleArea(base, height);
+            double a,b,c;
+            cout << "Enter a,b,c : ";
+            cin >> a >> b >> c ;
+            if(a+b>c&&a+c>b&&b+c>a){
+            double area = calculateTriangleArea(a,b,c);
             cout << "The area of the triangle is: " << area << endl;
             break;
+			}else{
+				cout<<"don 't have result"<<endl;
+				break;
+			}
         }
         case 3: {
             double radius;
             cout << "Enter the radius of the circle: ";
             cin >> radius;
+            if(radius>0){
             double area = calculateCircleArea(radius);
             cout << "The area of the circle is: " << area << endl;
-            break;
+            break;}
+            else 
+            	cout<<"Can't input negative number and zero"<<endl;
         }
         default:
             cout << "Invalid choice." << endl;
