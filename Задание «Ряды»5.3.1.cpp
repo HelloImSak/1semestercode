339d062e1c8177b46af87e0138dffd88e6c9f675
HelloImSak <140347848+HelloImSak@users.noreply.github.com>
2023-11-13 21:16:04 +0300

    Update Задание «Ряды»5.3.1.cpp

--- Задание «Ряды»5.3.1.cpp
+++ Задание «Ряды»5.3.1.cpp
@@ -18,3 +18,28 @@ int main() {
 
     return 0;
 }
+---------------------------------------------------------------------------------
+    #include <iostream>
+#include <cmath>
+
+int main() {
+    int n;
+    std::cout << "Enter the value of n: ";
+    std::cin >> n;
+    double y = 0.0;
+    double d = 0.0;
+
+    for (int i = 1; i <= n; ++i) {
+        if (i < 0) {
+            d += sin(static_cast<double>(i) * 3.14);
+        }
+        else {
+            d += sin(i);
+        }
+        y += static_cast<double>(i) / d;
+    }
+
+    std::cout << "Result: " << y << std::endl;
+
+    return 0;
+}
