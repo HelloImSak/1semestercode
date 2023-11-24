0d2c9e18ca6a30330442a1f53e34fc83e2dd2d1a
HelloImSak <140347848+HelloImSak@users.noreply.github.com>
2023-11-24 11:06:55 +0300

    Update Задача «Автоматный распознаватель»4.5.cpp

--- Задача «Автоматный распознаватель»4.5.cpp
+++ Задача «Автоматный распознаватель»4.5.cpp
@@ -1,43 +1,51 @@
 #include <iostream>
 #include <unordered_map>
+#include <cctype>
+
 using namespace std;
 
 int romanToDecimal(string roman) {
-    unordered_map<char, int> romanValues;
-    romanValues['I'] = 1;
-    romanValues['V'] = 5;
-    romanValues['X'] = 10;
-    romanValues['L'] = 50;
-    romanValues['C'] = 100;
-    romanValues['D'] = 500;
-    romanValues['M'] = 1000;
+    unordered_map<char, int> romanMap = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
 
     int result = 0;
-    int prevValue = 0;
+    int preValue = 0;
 
-    for (int i = roman.size() - 1; i >= 0; i--) {
-        int currentValue = romanValues[roman[i]];
+    for (int i = roman.length() - 1; i >= 0; --i) {
+        char currentChar = toupper(roman[i]);
+        int curValue = romanMap[currentChar];
 
-        if (currentValue < prevValue) {
-            result -= currentValue;
+        if (curValue < preValue) {
+            result -= curValue;
+        } else {
+            result += curValue;
+            preValue = curValue;
         }
-        else {
-            result += currentValue;
-        }
-
-        prevValue = currentValue;
     }
 
     return result;
 }
 
 int main() {
-    string roman;
-    cout << "Input the roman number: ";
-    cin >> roman;
+    string romanNumber;
+    
+    cout << "Enter a Roman numeral: ";
+    cin >> romanNumber;
 
-    int decimal = romanToDecimal(roman);
-    cout << "Decimal value: " << decimal << endl;
+    int decimalNumber = romanToDecimal(romanNumber);
+    
+    if (decimalNumber == 0) {
+        cout << "Invalid Roman numeral entered." << endl;
+    } else {
+        cout << "The decimal equivalent is: " << decimalNumber << endl;
+    }
 
     return 0;
 }
