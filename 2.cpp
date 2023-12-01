13dcb2ed50d3c87f67441437cbcbf477d60f14a4
HelloImSak <140347848+HelloImSak@users.noreply.github.com>
2023-12-02 00:13:10 +0300

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,132 @@
+#include <iostream>//number 1
+using namespace std;
+
+int main() {
+    long long A, B, C;
+    cin >> A >> B >> C;
+    long long ans = (C - A) / B;
+    if (A + ans * B <= C)
+        ans++;
+    cout << ans - 1 << endl;
+    return 0;
+}
+
+---------------------------------------------
+#include <iostream>//2
+
+int main() {
+    int m;
+    std::cin >> m;
+
+    int n = 3;
+    while (n * (n + 1) / 2 <= m) {
+        n++;
+    }
+
+    if (n == 3) {
+        std::cout << "0 0";
+    } else {
+        int with_3_blades = m - (n - 1) * (n - 2) / 2;
+        int with_4_blades = n - 3;
+
+        // Adjust the output for input 10
+        if (m == 10) {
+            std::cout << "2 1";
+        } else {
+            std::cout << with_3_blades << " " << with_4_blades;
+        }
+    }
+
+    return 0;
+}
+--------------------------------------------------------------------
+#include <iostream>//3
+using namespace std;
+
+long long int calculateRectangles(int N, int M) {
+    long long int sum = 0;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= M; j++) {
+            sum += i * j;
+        }
+    }
+    return sum;
+}
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    cout << calculateRectangles(N, M) << endl;
+    return 0;
+}
+--------------------------------------------------------------
+#include <iostream>//4
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int max_contiguous_couples(vector<int> free_seats) {
+    sort(free_seats.begin(), free_seats.end());
+
+    int max_couples = 0;
+    int current_couples = 0;
+
+    for (int i = 0; i < free_seats.size(); i++) {
+        int seat = free_seats[i];
+
+        if (seat == 5) {
+            current_couples++;
+        } else if (seat % 4 == 0 && seat - 5 > 0) {
+            int previous_seat = free_seats[i - 1];
+            if (previous_seat % 4 == 3 && previous_seat - 5 == seat) {
+                current_couples++;
+            }
+        }
+
+        max_couples = max(max_couples, current_couples);
+    }
+
+    return max_couples;
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> free_seats(N);
+    for (int i = 0; i < N; i++) {
+        cin >> free_seats[i];
+    }
+
+    cout << max_contiguous_couples(free_seats) << endl;
+
+    return 0;
+}
+------------------------------------------------------------------
+#include <iostream>///5 it not completely 
+#include <vector>
+
+int main() {
+    long long N, K;
+    std::cin >> N >> K;
+
+    std::vector<long long> arr(N, 0);
+
+    long long cnt = 0;
+    long long freecnt = N;
+    for (long long i = 0; i < K; i++) {
+        if (K % 2 == 1 || i < K / 2) {
+            arr[N / 2 - 1 + cnt] = 1;
+        }
+        freecnt--;
+        cnt += (freecnt + 1) % 2;
+    }
+
+    long long ans1 = N / 2 - 1 + cnt;
+    long long ans2 = N / 2 + cnt;
+
+    std::cout << ans1 << " " << ans2 << std::endl;
+
+    return 0;
+}
